7c17a7ce0bdde88cfd9de2d3fdd1e2c932d3145e
Rubens Takauti <rtakauti@hotmail.com>
2020-09-14 23:09:28 -0300

    Ajusta DTOs

---
+++ app/DTO/AbstractDTO.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Rtakauti\DTO;
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+use JsonSerializable;
+
+abstract class AbstractDTO implements JsonSerializable
+{
+
+    protected array $attribute;
+
+    public function __construct(array $attribute)
+    {
+        $this->attribute = $attribute;
+    }
+
+    public function jsonSerialize()
+    {
+        try {
+            return json_encode($this->attribute, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        } catch (Exception $exception) {
+            die($exception->getMessage());
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed|string
+     * @throws Exception
+     */
+    public function __call($name, $arguments)
+    {
+        $attribute = $this->cleanGet($name);
+        $class = __NAMESPACE__ . '\\' . ucfirst($attribute) . 'DTO';
+        if (empty($result = $this->attribute[$attribute])) {
+            return '';
+        }
+        if (is_array($result)) {
+            return new $class($result);
+        }
+        if (is_numeric($result)) {
+            return $result;
+        }
+        try {
+            if (!is_bool($date = new DateTime($result, new DateTimeZone('America/Sao_Paulo')))) {
+                return $date;
+            }
+        } catch (Exception $e) {
+        }
+        return $result;
+    }
+
+    /**
+     * @param string $method
+     * @param string $prefix
+     * @return string
+     */
+    protected function cleanGet(string $method, string $prefix = 'get'): string
+    {
+        return (strpos($method, $prefix) !== false) ? lcfirst(str_replace($prefix, '', $method)) : '';
+    }
+}
--- app/Support/PropertyList.php
+++ app/Support/PropertyList.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Rtakauti\Support;
 
 
+use Exception;
 use Rtakauti\DTO\PropertyDTO;
 
 class PropertyList extends Collection
 {
+    use PaginateableTrait;
+
     public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
@@ -20,11 +23,19 @@ class PropertyList extends Collection
 
     public function offsetGet($offset): PropertyDTO
     {
-        return new PropertyDTO(parent::offsetGet($offset));
+        try {
+            return new PropertyDTO(json_decode(parent::offsetGet($offset), true, 512, JSON_THROW_ON_ERROR));
+        } catch (Exception $exception) {
+            die($exception->getMessage());
+        }
     }
 
     public function current(): PropertyDTO
     {
-        return new PropertyDTO(parent::current());
+        try {
+            return new PropertyDTO(json_decode(parent::current(), true, 512, JSON_THROW_ON_ERROR));
+        } catch (Exception $exception) {
+            die($exception->getMessage());
+        }
     }
 }
